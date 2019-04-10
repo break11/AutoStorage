08f37bd2e265bf12c687929136e983bb60bb1cb5
Rus Sh <break1@yandex.ru>
2019-04-10 18:39:21 +0300

    подготовка в агентс менеджере к вводу роут билдера

--- App/AgentsManager/mainwindow.py
+++ App/AgentsManager/mainwindow.py
@@ -4,16 +4,17 @@ import sys
 import os
 import networkx as nx
 import time
+import weakref
 
 from copy import deepcopy
-from PyQt5.QtCore import (pyqtSlot, QByteArray, QTimer, Qt)
-from PyQt5.QtGui import (QStandardItemModel, QStandardItem)
-from PyQt5.QtWidgets import (QGraphicsView, QGraphicsScene, QMainWindow, QFileDialog, QMessageBox, QAction)
+from PyQt5.QtCore import pyqtSlot, QByteArray, QTimer, Qt, QAbstractTableModel, QModelIndex
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QMainWindow, QFileDialog, QMessageBox, QAction
 from PyQt5 import uic
 
 from Lib.Common.SettingsManager import CSettingsManager as CSM
 from Lib.Common import FileUtils
-from Lib.Common.Agent_NetObject import CAgent_NO, def_props
+from Lib.Common.Agent_NetObject import CAgent_NO, def_props, s_edge, s_position, s_route, s_route_idx, s_angle
 import Lib.Common.StrConsts as SC
 from Lib.Common.GuiUtils import time_func, load_Window_State_And_Geometry, save_Window_State_And_Geometry
 from Lib.Net.NetObj import CNetObj
@@ -23,6 +24,70 @@ from .AgentsMoveManager import CAgents_Move_Manager
 from Lib.Common.Agent_NetObject import agentsNodeCache
 from Lib.Common.Graph_NetObjects import graphNodeCache
 from Lib.Common.GraphUtils import tEdgeKeyFromStr, tEdgeKeyToStr
+from Lib.Net.Net_Events import ENet_Event as EV
+
+class CAgents_Model( QAbstractTableModel ):
+    propList = [ "name", "UID", s_edge, s_position, s_route, s_route_idx ]
+
+    def __init__( self, parent ):
+        super().__init__( parent=parent)
+        self.agentsNode = agentsNodeCache()
+
+        self.agentsList = [ agentNO.UID for agentNO in self.agentsNode().children ]
+
+        CNetObj_Manager.addCallback( EV.ObjCreated, self.onObjCreated )
+        CNetObj_Manager.addCallback( EV.ObjPrepareDelete, self.onObjPrepareDelete )
+
+    def rowCount( self, parentIndex ):
+        # return len( self.agentsNode().children ) if self.agentsNode() else 0
+        return len( self.agentsList )
+
+    def columnCount( self, parentIndex ):
+        return len( self.propList )
+    
+    def data( self, index, role ):
+        if not index.isValid(): return None
+
+        # netObj = list(self.agentsNode().children)[ index.row() ]
+        objUID = self.agentsList[ index.row() ]
+        netObj = CNetObj_Manager.accessObj( objUID, genAssert=True )
+        sPropName = self.propList[ index.column() ]
+
+        if role == Qt.DisplayRole or role == Qt.EditRole:            
+            return getattr( netObj, sPropName ) if netObj else None
+
+    def headerData( self, section, orientation, role ):
+        if role != Qt.DisplayRole: return
+
+        if orientation == Qt.Horizontal:
+            return self.propList[ section ]
+
+    def flags( self, index ):
+        return Qt.ItemIsSelectable | Qt.ItemIsEnabled | Qt.ItemIsEditable
+
+    def onObjCreated( self, netCmd ):
+        netObj = CNetObj_Manager.accessObj( netCmd.Obj_UID, genAssert=True )
+        if netObj.parent != self.agentsNode(): return
+
+        idx = len( self.agentsList )
+        self.beginInsertRows( QModelIndex(), idx, idx )
+        self.agentsList.append( netObj.UID )
+        self.endInsertRows()
+
+        self.layoutChanged.emit()
+
+    def onObjPrepareDelete( self, netCmd ):
+        netObj = CNetObj_Manager.accessObj( netCmd.Obj_UID, genAssert=True )
+        if netObj.parent != self.agentsNode(): return
+
+        idx = self.agentsList.index( netObj.UID )
+        print( "idx = ", idx )
+        self.beginRemoveRows( QModelIndex(), idx, idx )
+        # self.agentsList.remove( netObj.UID )
+        del self.agentsList[ idx ]
+        self.endRemoveRows()
+
+        self.layoutChanged.emit()
 
 class CAM_MainWindow(QMainWindow):
     def __init__(self):
@@ -35,20 +100,35 @@ class CAM_MainWindow(QMainWindow):
         self.SimpleAgentTest_Timer.timeout.connect( self.SimpleAgentTest )
 
         self.graphRootNode = graphNodeCache()
-        self.agentsNodeCache = agentsNodeCache()
+        self.agentsNode = agentsNodeCache()
                 
     def init( self, initPhase ):
         if initPhase == EAppStartPhase.BeforeRedisConnect:
             load_Window_State_And_Geometry( self )
+        elif initPhase == EAppStartPhase.AfterRedisConnect:
+            self.Agents_Model = CAgents_Model( parent = self )
+            self.tvAgents.setModel( self.Agents_Model )
+            self.tvTest.setModel( self.Agents_Model )
 
     def closeEvent( self, event ):
         save_Window_State_And_Geometry( self )
 
-    @pyqtSlot("bool")
-    def on_btnAddAgent_clicked( self, bVal ):
+    def on_btnAddAgent_released( self ):
         props = deepcopy( def_props )
-        AgentsNode = CNetObj.resolvePath( CNetObj_Manager.rootObj, "Agents" )
-        CAgent_NO( parent=AgentsNode, props=props )
+        # AgentsNode = CNetObj.resolvePath( CNetObj_Manager.rootObj, "Agents" )
+        CAgent_NO( parent=self.agentsNode(), props=props )
+
+    def on_btnDelAgent_released( self ):
+        ci = self.tvAgents.currentIndex()
+        print( ci.isValid(), ci.row() )
+        if not ci.isValid(): return
+        
+        # print( ci.row() )
+        agentNetObj = list( self.agentsNode().children )[ ci.row() ]
+        agentNetObj.destroy()
+
+        self.tvAgents.setCurrentIndex( ci )
+
 
     ###################################################
 
@@ -89,7 +169,7 @@ class CAM_MainWindow(QMainWindow):
 
     def SimpleAgentTest( self ):
         if self.graphRootNode() is None: return
-        if self.agentsNodeCache().childCount() == 0: return
+        if self.agentsNode().childCount() == 0: return
 
-        for agentNO in self.agentsNodeCache().children:
+        for agentNO in self.agentsNode().children:
             self.AgentTestMoving( agentNO )
