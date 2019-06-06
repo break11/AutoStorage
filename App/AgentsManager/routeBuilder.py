bd85ecab713352441c916a457ae2acb8471717de
Alex NRG <brd1080@yandex.ru>
2019-06-06 18:11:39 +0300

    Переименование rail в segment

--- App/AgentsManager/routeBuilder.py
+++ App/AgentsManager/routeBuilder.py
@@ -64,7 +64,7 @@ class SI_Item:
         return f"< SII (length={self.length} K={self.K} edge={self.edge} pos={self.pos}) >\n"
 
 
-class SRailSegment:
+class CRailSegment:
     def __init__(self, length, railHeight, sensorSide, widthType, curvature):
         self.length = length
         self.railHeight = railHeight
@@ -102,79 +102,78 @@ class CRouteBuilder():
             return commands, SegmentsInfoItems
 
         # 0) Split path by fractures
-        pathParts = self.splitPathByFractures(nodeList)
+        Sequences = self.splitPathByFractures(nodeList)
 
         angle = agent_angle
-        for pathPart in pathParts:
-            angle, direction = self.getDirection( (pathPart[0], pathPart[1]), angle )
+        for single_sequence in Sequences:
+            angle, direction = self.getDirection( (single_sequence[0], single_sequence[1]), angle )
             """
             path part is a node sequence with constans rail width and direction of movement. 
             Shuttle should start to move at the beginning of pathPart, and do full stop at the end 
             """
             # 1) generate rails from edges
-            rails = self.nodeListToRails(pathPart)
-
-            railStartCurvature = rails[0].curvature
+            segments = self.nodeListToSegments( single_sequence )
+            startSegmentCurv = segments[0].curvature
 
             # 2) add ledge
-            ledgeNode = self.findNodeForLedge(pathPart[-2], pathPart[-1])
-            ledgeRailList = self.nodeListToRails([pathPart[-1], ledgeNode])
+            ledgeNode = self.findNodeForLedge(single_sequence[-2], single_sequence[-1])
+            ledgeSegments = self.nodeListToSegments([single_sequence[-1], ledgeNode])
 
-            ledgeSize = self.LedgeSizeByEdge( ( pathPart[-2], pathPart[-1] ) )
+            ledgeSize = self.LedgeSizeByEdge( ( single_sequence[-2], single_sequence[-1] ) )
 
-            ledge = self.takeRailListPart( ledgeRailList, ledgeSize )
-            railsWithLedge = rails + ledge
-
+            ledge = self.takeSegmentsFromBegin( ledgeSegments, ledgeSize )
+            SegmentsWithLedge = segments + ledge
 
             # 3) truncate the path by agen's width or length, depending on the current width, (because agents move by sensors)
-            railListWithLedgeCuttedFromBegin = self.cutRailListFromBegin(railsWithLedge, ledgeSize)
+            SegmentsWithLedgeCuttedFromBegin = self.cutSegmentsFromBegin(SegmentsWithLedge, ledgeSize)
 
-            sourceRailList =  self.glueSameRailListParts(railListWithLedgeCuttedFromBegin)
-            SegmentsInfoItems_pathPart = [ SI_Item( length = railSegment.length, K = dirToK[ direction ], edge="", pos=0 ) for railSegment in sourceRailList ]
+            sourceSegments =  self.mergeSameSegments(SegmentsWithLedgeCuttedFromBegin)
+            SegmentsInfoItems_Sequence = [ SI_Item( length = segment.length, K = dirToK[ direction ], edge="", pos=0 ) for segment in sourceSegments ]
             
             # 4) shift the path by hysteresis
-            railListWithHystShift = self.shiftRailListByHyst( railListWithLedgeCuttedFromBegin )
-            # railListWithHystShift = railListWithLedgeCuttedFromBegin
+            SegmentsWithHystShift = self.shiftSegmentsByHyst( SegmentsWithLedgeCuttedFromBegin )
 
             # 5) adjust the curvature at the beggining of the path
-            railListWithAdjustedCurvature = railListWithHystShift
-            if railStartCurvature == SGT.ECurvature.Curve.name:
-                railListWithAdjustedCurvature = self.setCurvatureFromBegin(railListWithHystShift, ledgeSize, railStartCurvature)
+            SegmentsWithAdjustedCurvature = SegmentsWithHystShift
+
+            if startSegmentCurv == SGT.ECurvature.Curve:
+                SegmentsWithAdjustedCurvature = self.setCurvatureFromBegin( SegmentsWithHystShift, ledgeSize, startSegmentCurv )
 
             # 6) merge all the rails with the same shape
-            gluedRailList = self.glueSameRailListParts(railListWithAdjustedCurvature)
+            mergedSegments = self.mergeSameSegments( SegmentsWithAdjustedCurvature )
 
             # 7) Add delta to rails when rail type change exists at the end of a segment
-            railListWithAddedDelta = self.addDeltaToRailList( gluedRailList )
-
-            commands.append( self.gluedRailListToCommands(railListWithAddedDelta, direction))
+            SegmentsWithDelta = self.addDeltaToSegments( mergedSegments )
+            commands.append( self.SegmentsToCommands( SegmentsWithDelta, direction ))
 
             # Доворачивание угла проходом по всем граням сиквенса, для корректного определения направления на след. итерации расчета
-            for i in range( len(pathPart) -1  ):
-                angle, direction = self.getDirection( (pathPart[i], pathPart[i+1]), angle )
-
-            # выявление позиций для DE
-            edgesList = edgesListFromNodes( pathPart )
-            edgesList.append( (pathPart[-1], ledgeNode) )
-            startEdgeIdx = 0
-
-            l = ledgeSize
-            for SII in SegmentsInfoItems_pathPart:
-                l = l + SII.length
-                for i in range( startEdgeIdx, len(edgesList) ):
-                    tKey = edgesList[ i ]
-                    edgeSize = self.edgeSize( tKey )
-                    if edgeSize < l:
-                        l -= edgeSize
-                        continue
-                    startEdgeIdx = i
-                    
-                    SII.edge, SII.pos = self.shiftPos( edgesList, tKey, l, -ledgeSize )
-                    break
-
-            SegmentsInfoItems += SegmentsInfoItems_pathPart
-
+            for i in range( len(single_sequence) -1  ):
+                angle, direction = self.getDirection( (single_sequence[i], single_sequence[i+1]), angle )
+        
+            self.calc_DE_Pos( SegmentsInfoItems_Sequence, single_sequence, ledgeNode,  ledgeSize )
+            SegmentsInfoItems += SegmentsInfoItems_Sequence
+        
         return commands, SegmentsInfoItems
+
+    def calc_DE_Pos(self, SII_Sequince, single_sequence, ledgeNode,  ledgeSize):
+        # выявление позиций для DE
+        edgesList = edgesListFromNodes( single_sequence )
+        edgesList.append( (single_sequence[-1], ledgeNode) )
+        startEdgeIdx = 0
+
+        l = ledgeSize
+        for SII in SII_Sequince:
+            l = l + SII.length
+            for i in range( startEdgeIdx, len(edgesList) ):
+                tKey = edgesList[ i ]
+                edgeSize = self.edgeSize( tKey )
+                if edgeSize < l:
+                    l -= edgeSize
+                    continue
+                startEdgeIdx = i
+                
+                SII.edge, SII.pos = self.shiftPos( edgesList, tKey, l, -ledgeSize )
+                break
 
     def getDirection(self, tEdgeKey, agent_angle):
         DirDict = { True: SGT.EDirection.R, False: SGT.EDirection.F, None: SGT.EDirection.Error }
@@ -244,50 +243,50 @@ class CRouteBuilder():
         
         return bestNode
 
-    def takeRailListPart(self, railList, length):
+    def takeSegmentsFromBegin(self, Segments, length):
         # function takes railList and return only part of it with specified length
         outRailList = []
-        for rail in railList:
-            if rail.length < length:
-                outRailList.append(rail)
-                length = length - rail.length
+        for segment in Segments:
+            if segment.length < length:
+                outRailList.append(segment)
+                length = length - segment.length
             else:
-                cuttedRailSegment = deepcopy( rail )
-                cuttedRailSegment.length = length
-                outRailList.append( cuttedRailSegment )
+                cuttedSegment = deepcopy( segment )
+                cuttedSegment.length = length
+                outRailList.append( cuttedSegment )
                 return outRailList
         return outRailList
 
-    def cutRailListFromBegin(self, railList, length):
+    def cutSegmentsFromBegin(self, Segments, length):
         # function takes railList and cuts a part of it with specified length from the beginning
-        outRailList = []
+        outSegments = []
         self.length = length
-        for rail in railList:
-            if rail.length < self.length:
+        for segment in Segments:
+            if segment.length < self.length:
                 #drop this rail
-                self.length = self.length - rail.length
+                self.length = self.length - segment.length
             else:
                 if self.length > 0:
-                    cuttedRailSegment = deepcopy(rail)
-                    cuttedRailSegment.length = rail.length - self.length
-                    outRailList.append(cuttedRailSegment)
+                    cuttedSegment = deepcopy(segment)
+                    cuttedSegment.length = segment.length - self.length
+                    outSegments.append(cuttedSegment)
                     self.length = 0
                 else:
-                    outRailList.append(rail)
+                    outSegments.append(segment)
 
-        return outRailList
+        return outSegments
 
-    def setCurvatureFromBegin(self, railList, length, curvature):
+    def setCurvatureFromBegin(self, Segments, length, curvature):
         lengthSetted = 0
-        for rail in railList:
-            rail.curvature = curvature
-            lengthSetted += rail.length
+        for segment in Segments:
+            segment.curvature = curvature
+            lengthSetted += segment.length
             if lengthSetted >= length:
-                return railList
+                return Segments
 
 
-    def nodeListToRails(self, path):
-        rails = []
+    def nodeListToSegments(self, path):
+        Segments = []
         if len(path) >= 1:
             for i in range(0, len(path) - 1):
                 n0 = path[i]
@@ -301,56 +300,56 @@ class CRouteBuilder():
                 curvature        = SGT.ECurvature.fromString ( edge[ SGT.s_curvature  ] )
 
                 if highRailSizeFrom > 0:
-                    railSegment = SRailSegment(highRailSizeFrom, RH_HIGH, sensorSide, widthType, curvature)                    
-                    rails.append(railSegment)
+                    segment = CRailSegment(highRailSizeFrom, RH_HIGH, sensorSide, widthType, curvature)                    
+                    Segments.append(segment)
 
-                rail_seg_length = edgeSize - highRailSizeFrom - highRailSizeTo
-                if rail_seg_length > 0 :
-                    railSegment = SRailSegment(rail_seg_length, RH_LOW, sensorSide, widthType, curvature)
-                    rails.append(railSegment)
+                seg_length = edgeSize - highRailSizeFrom - highRailSizeTo
+                if seg_length > 0 :
+                    segment = CRailSegment(seg_length, RH_LOW, sensorSide, widthType, curvature)
+                    Segments.append(segment)
 
                 if highRailSizeTo > 0:
-                    railSegment = SRailSegment(highRailSizeTo, RH_HIGH, sensorSide, widthType, curvature)
-                    rails.append(railSegment)
-        return rails
+                    segment = CRailSegment(highRailSizeTo, RH_HIGH, sensorSide, widthType, curvature)
+                    Segments.append(segment)
+        return Segments
 
-    def glueSameRailListParts(self, railList):
+    def mergeSameSegments(self, Segments):
         outRailList = []
-        length     = railList[0].length
-        railHeight = railList[0].railHeight
-        sensorSide = railList[0].sensorSide
-        widthType  = railList[0].widthType
-        curvature  = railList[0].curvature
+        length     = Segments[0].length
+        railHeight = Segments[0].railHeight
+        sensorSide = Segments[0].sensorSide
+        widthType  = Segments[0].widthType
+        curvature  = Segments[0].curvature
 
-        for rail in railList[1:]:
-            if (rail.railHeight == railHeight) and (rail.sensorSide == sensorSide) and (rail.curvature == curvature):
-                length = length + rail.length
+        for segment in Segments[1:]:
+            if (segment.railHeight == railHeight) and (segment.sensorSide == sensorSide) and (segment.curvature == curvature):
+                length = length + segment.length
             else:
-                newRail = SRailSegment(length, railHeight, sensorSide, widthType, curvature)
+                newRail = CRailSegment(length, railHeight, sensorSide, widthType, curvature)
                 outRailList.append(newRail)
 
-                length     = rail.length
-                railHeight = rail.railHeight
-                sensorSide = rail.sensorSide
-                widthType  = rail.widthType
-                curvature  = rail.curvature
+                length     = segment.length
+                railHeight = segment.railHeight
+                sensorSide = segment.sensorSide
+                widthType  = segment.widthType
+                curvature  = segment.curvature
 
-        newRail = SRailSegment(length, railHeight, sensorSide, widthType, curvature)
+        newRail = CRailSegment(length, railHeight, sensorSide, widthType, curvature)
         outRailList.append(newRail)
         return outRailList
 
-    def shiftRailListByHyst(self, railList):
+    def shiftSegmentsByHyst(self, Segments):
         # encrease first segment by hyst, decrease last segment by hyst
-        railList[0].length  = railList[0].length + hysteresis
-        railList[-1].length = railList[-1].length - hysteresis
+        Segments[0].length  = Segments[0].length + hysteresis
+        Segments[-1].length = Segments[-1].length - hysteresis
 
         # если длина рельса была меньше, чем величина гистерезиса, компенсируем отрицательное значение предыдущими рельсами 
-        while railList[-1].length < 0:
-            d = railList[-1].length
-            del railList[-1]
-            railList[-1].length += d
+        while Segments[-1].length < 0:
+            d = Segments[-1].length
+            del Segments[-1]
+            Segments[-1].length += d
 
-        return railList
+        return Segments
 
     def shiftPos(self, edgesList, tKey, pos, delta ):
         edgeIDX = edgesList.index(tKey)
@@ -376,34 +375,34 @@ class CRouteBuilder():
                 return edgesList[edgeIDX], pos
 
     
-    def addDeltaToRailList(self, railList):
+    def addDeltaToSegments(self, Segments):
         # adding delta to rail size to take into account possible wheels slippage, etc.
         # delta only should be added for rail segment with rail height change at the end
-        for i in range(0, len(railList)-1 ):
-            rail = railList[i]
-            railNext = railList[i+1]
+        for i in range(0, len(Segments)-1 ):
+            segment = Segments[i]
+            nextSegment = Segments[i+1]
 
-            if rail.railHeight != railNext.railHeight:
-                length = rail.length
+            if segment.railHeight != nextSegment.railHeight:
+                length = segment.length
                 delta = length * (shiftFract/100.0)
                 delta = shiftLeast if delta < shiftLeast else delta
                 length = length + delta
-                rail.length = length
+                segment.length = length
         
-        return railList
+        return Segments
 
-    def gluedRailListToCommands(self, railList, temp__direction):
+    def SegmentsToCommands(self, Segments, temp__direction):
         commands = []
-        widthTypeChar = widthTypeToChar[ railList[0].widthType ]
+        widthTypeChar = widthTypeToChar[ Segments[0].widthType ]
         direction = temp__direction
         commands.append('@SB')
         commands.append( f"@WO:{widthTypeChar}" )
 
-        for rail in railList:
-            lengthStr = ('{:06d}').format(int(rail.length))
-            railHeightStr = railHeightToChar[rail.railHeight]
-            sensorSideStr = sensorSideToChar[ (rail.sensorSide, direction) ]
-            curvatureChar = curvatureToChar[ rail.curvature ]
+        for segment in Segments:
+            lengthStr = ('{:06d}').format(int(segment.length))
+            railHeightStr = railHeightToChar[segment.railHeight]
+            sensorSideStr = sensorSideToChar[ (segment.sensorSide, direction) ]
+            curvatureChar = curvatureToChar[ segment.curvature ]
             directionChar = directionToChar[ direction ]
             command = f"@DP:{lengthStr},{ directionChar },{railHeightStr},{sensorSideStr},{curvatureChar}"
             commands.append( command )
