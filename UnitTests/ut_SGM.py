d82d21d4527212763a8cefd6a8499c0a2dcb0fda
Alex NRG <brd1080@yandex.ru>
2019-06-21 17:37:43 +0300

    Заготовка для юнит-теста calcNodeMiddleLine

--- UnitTests/ut_SGM.py
+++ UnitTests/ut_SGM.py
@@ -3,28 +3,54 @@ import unittest
 
 import sys
 import os
+import networkx as nx
 
 sys.path.append( os.path.abspath(os.curdir)  )
 
-from PyQt5.QtWidgets import QGraphicsView, QWidget
-from PyQt5.QtWidgets import QApplication
+# from PyQt5.QtWidgets import QGraphicsView, QWidget
+# from PyQt5.QtWidgets import QApplication
 
 from Lib.StorageViewer.StorageGraph_GScene_Manager import CStorageGraph_GScene_Manager
-from Lib.Common.GridGraphicsScene import CGridGraphicsScene
-from Lib.Common.BaseApplication import CBaseApplication
-from Lib.Net.NetObj_Manager import CNetObj_Manager
+from Lib.Common import StorageGraphTypes as SGT
+from Lib.Common.GraphUtils import loadGraphML_File
+sDir = "./GraphML/"
+
+class CNode_SGItem_Dummy:
+    def __init__(self, nodeID):
+        self.nodeID = nodeID
+        self.nodeType = SGT.ENodeTypes.NoneType
+        self.middleLineAngle = None
+
+    def setMiddleLineAngle(self, fVal):
+        self.middleLineAngle = fVal
+
+class CSGM_Dummy:
+    def __init__(self, sFName):
+        self.nodeGItems     = {}
+        self.edgeGItems     = {}
+
+        self.nxGraph = loadGraphML_File(sFName)
+
+        ####
+        for nodeID in self.nxGraph.nodes():
+            dummyNodeItem = CNode_SGItem_Dummy(nodeID)
+            self.nodeGItems[nodeID] = dummyNodeItem
+            try:
+                if self.nxGraph.nodes()[nodeID][SGT.s_nodeType] == SGT.ENodeTypes.StorageSingle.name:
+                    dummyNodeItem.nodeType = SGT.ENodeTypes.StorageSingle
+            except:
+                pass
+
+        ####
+        for tKey in self.nxGraph.edges():
+            self.edgeGItems[ frozenset(tKey) ] = True
+
+    def calcNodeMiddleLine(self, dummyNodeItem):
+        CStorageGraph_GScene_Manager.calcNodeMiddleLine(self, dummyNodeItem)
 
 
-# app = CBaseApplication( sys.argv, bNetworkMode = False )
+SGM = CSGM_Dummy ( sDir + "test_storage_rotation.graphml" )
 
-# sDir = "./GraphML/"
-
-# CNetObj_Manager.initRoot()
-
-# parent = QWidget()
-# StorageMap_Scene = CGridGraphicsScene(parent)
-# StorageMap_View  = QGraphicsView()
-
-# SGM = CStorageGraph_GScene_Manager( StorageMap_Scene, StorageMap_View )
-# SGM.init()
-# SGM.load( sDir + "test_storage_rotation.graphml" )
+for dummyNodeItem in SGM.nodeGItems.values():
+    SGM.calcNodeMiddleLine( dummyNodeItem )
+    # print ( dummyNodeItem.nodeID, dummyNodeItem.middleLineAngle )
