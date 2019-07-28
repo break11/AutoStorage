dc11be9eec9f7b1e41bdfc767810bc196135c0cc
break1-Home <break1@yandex.ru>
2019-07-28 04:50:56 +0300

    ошибка - падение агент менеджера при получении команды OD, OP при незагруженном графе

--- App/AgentsManager/AgentLink.py
+++ App/AgentsManager/AgentLink.py
@@ -105,6 +105,7 @@ class CAgentLink( CAgentServer_Link ):
     def processRxPacket( self, cmd ):
         if cmd.event == EAgentServer_Event.OdometerZero:
             self.agentNO().odometer = 0
+
         elif cmd.event == EAgentServer_Event.BatteryState:
             agentNO = self.agentNO()
 
@@ -125,10 +126,11 @@ class CAgentLink( CAgentServer_Link ):
                 self.DE_IDX += 1
 
         elif cmd.event in [EAgentServer_Event.OdometerDistance, EAgentServer_Event.OdometerPassed]:
-            nxGraph = self.graphRootNode().nxGraph
-            if nxGraph is None:
+            if self.graphRootNode() is None:
                 print( f"{SC.sWarning} No Graph loaded." )
                 return
+
+            nxGraph = self.graphRootNode().nxGraph
 
             agentNO = self.agentNO()
 
