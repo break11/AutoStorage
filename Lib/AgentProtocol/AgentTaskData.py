2573d3b532e1ab4aafafab5f40e586ef1c2243fc
Rus Sh <break1@yandex.ru>
2019-10-25 16:01:51 +0300

    распознавание данных для заданий в списке заданий челнока

--- Lib/AgentProtocol/AgentTaskData.py
+++ Lib/AgentProtocol/AgentTaskData.py
@@ -3,6 +3,9 @@ from enum import auto
 from Lib.Common.BaseEnum import BaseEnum
 # from Lib.AgentProtocol.AgentDataTypes import 
 
+TS  = "," # Task Splitter
+TDS = "=" # Task Data Splitter
+
 class ETaskType( BaseEnum ):
     Undefined = auto()
     GoToPoint = auto()
@@ -11,6 +14,15 @@ class ETaskType( BaseEnum ):
     Default = Undefined
 
 class CTask:
+    dataFromStrFunc = {
+                        ETaskType.Undefined : lambda sData : sData,
+                        ETaskType.GoToPoint : lambda sData : int( sData )
+                      }
+    dataToStrFunc   = {
+                        ETaskType.Undefined : lambda data : data,
+                        ETaskType.GoToPoint : lambda data : str( data )
+                      }
+
     def __init__( self, taskType, taskData=None ):
         self.taskType = taskType
         self.taskData = taskData
@@ -19,10 +31,10 @@ class CTask:
 
     @classmethod
     def fromString( cls, data ):
-        l = data.split( ":" )
+        l = data.split( TDS )
         taskType = ETaskType.fromString( l[0] )
         if len( l ) > 1:
-            taskData = taskData = l[1]
+            taskData = cls.dataFromString( taskType, l[1] )
         else:
             taskData = None
         return CTask( taskType = taskType, taskData = taskData )
@@ -30,9 +42,25 @@ class CTask:
     def toString( self ):
         sR = f"{self.taskType}"
         if self.taskData is not None:
-            sR = f"{sR}:{self.taskData}"
+            sR = f"{sR}{TDS}{self.dataToString()}"
+        print( sR )
         return sR
     
+    @classmethod
+    def dataFromString( cls, taskType, sData ):
+        if taskType in cls.dataFromStrFunc.keys():
+            try:
+                return cls.dataFromStrFunc[ taskType ]( sData )
+            except:
+                return None
+        else:
+            return None
+
+    def dataToString( self ):
+        if self.taskType in self.dataToStrFunc.keys():
+            return self.dataToStrFunc[ self.taskType ]( self.taskData )
+        else:
+            return None
 
 class CTaskList:
     def __init__( self, taskList=None ):
@@ -46,12 +74,11 @@ class CTaskList:
             return CTaskList()
 
         rL = []
-        l = data.split( "," )
+        l = data.split( TS )
         for sTask in l:
             task = CTask.fromString( sTask )
             rL.append( task )
         return CTaskList( rL )
 
     def toString( self ):
-        # for task in self.taskList:
-        return ",".join( map(str, self.taskList) )
+        return TS.join( map(str, self.taskList) )
