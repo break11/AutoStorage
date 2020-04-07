b60a09d7f15ec20ce5c065fcd71b43c379f64616
break1-Home <break1@yandex.ru>
2020-04-07 17:58:53 +0300

    добавление поля bitNum на согласование для адресации внутри маски

--- Lib/Common/ModbusTypes.py
+++ Lib/Common/ModbusTypes.py
@@ -20,10 +20,11 @@ class CRegisterAddress:
     DS = "|"
     # "1|AI|51"
     
-    def __init__( self, unitID, _type, number, bValid = True ):
+    def __init__( self, unitID, _type, number, bitNum=None, bValid = True ):
         self.unitID = unitID
         self._type   = _type
         self.number = number
+        self.bitNum = bitNum
         self.bValid  = bValid
 
     def __str__( self ): return self.toString()
@@ -33,6 +34,7 @@ class CRegisterAddress:
         eq = eq and self.unitID == other.unitID
         eq = eq and self.type == other.type
         eq = eq and self.number == other.number
+        eq = eq and self.bitNum == other.bitNum
         eq = eq and self.bValid == other.bValid
         return eq
 
@@ -43,6 +45,7 @@ class CRegisterAddress:
             unitID = int( l[0] )
             _type = ERegisterType.fromString( l[1] )
             number = int( l[2] )
+            bitNum = int[ l[3] ] if len( l ) > 3 else None
             bValid = True
         except:
             print( f"{SC.sError} CRegisterAddress can't convert from '{data}'!" )
@@ -50,11 +53,14 @@ class CRegisterAddress:
             if _type is None:
                 _type = ERegisterType.Default
             number = 0
+            bitNum = 0
             bValid = False
 
-        return CRegisterAddress( unitID, _type, number, bValid )
+        return CRegisterAddress( unitID, _type, number, bitNum, bValid )
 
     def toString( self, bShortForm = False ):
         sResult = f"{self.unitID}{ self.DS }{self._type}{ self.DS }{self.number}"
+        if self.bitNum is not None:
+            sResult += f"{ self.DS }{self.bitNum}"
 
         return sResult
