a28c01c763115bb02dbd46a0129b516d1ea209c9
Alex NRG <brd1080@yandex.ru>
2020-04-09 17:55:06 +0300

    Вариации теста для pack_register_cache

--- Lib/Common/ModbusConnector_Funcs.py
+++ Lib/Common/ModbusConnector_Funcs.py
@@ -23,8 +23,9 @@ def pack_register_cache( cache_dict ):
     packed_cache = []
     regiser_addresses = sorted( cache_dict.keys() )
 
+    # пример regiser_addresses = [ 0, 1, 2, 13, 14, 15 ]
     # группировка адресов в контейнере вида [ (0, [(0,0)(1,1)(2,2)]), (-10, [(3,13)(4,14)(5,15)]) ]
-    # ключ группировки - разница между значением адреса и позицией в списке regiser_addresses:
+    # ключ группировки (здесь 0 и -10 ) - разница между значением адреса и позицией в списке regiser_addresses:
     address_groups = groupby( enumerate(regiser_addresses), lambda x: x[0]-x[1] )
     # приведение группировки к списку вида [ [1,2,3], [13,14,15] ]:
     address_groups = [ [e[1] for e in vals] for key, vals in address_groups ]
